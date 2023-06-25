209727ca7e4d30cd0d5d0d3637c7d63cb914a937
m-hassan-albitar <codermhassan@gmail.com>
2023-06-25 19:16:52 +0300

    Handel all exceptions Add new feature to learn only words you don't know

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 # LOGIC
-data = pd.read_csv("./data/french_words.csv")
-df = pd.DataFrame(data)
-data_list = df.to_dict(orient="records")
 current_card = {}
+try:
+    data = pd.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("./data/french_words.csv")
+    data_list = data.to_dict(orient="records")
+else:
+    data_list = data.to_dict(orient="records")
 
 
-def create_card():
+def next_card():
     global current_card, flip
     window.after_cancel(flip)
     current_card = random.choice(data_list)
@@ -24,9 +28,18 @@ def create_card():
 
 
 def change_card():
+    global current_card
     canvas.itemconfig(title, text="English", fill="white")
     canvas.itemconfig(word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_img, image=back_img)
+
+
+def to_learn():
+    global current_card
+    data_list.remove(current_card)
+    learn = pd.DataFrame(data_list)
+    learn.to_csv("./data/words_to_learn.csv", encoding=None)
+    next_card()
 
 
 # UI
@@ -49,12 +62,12 @@ canvas.grid(row=0, column=0, columnspan=2)
 right_img = PhotoImage(file="./images/right.png")
 wrong_img = PhotoImage(file="./images/wrong.png")
 
-right_btn = Button(image=right_img, highlightthickness=0, background=BACKGROUND_COLOR, command=create_card)
+right_btn = Button(image=right_img, highlightthickness=0, background=BACKGROUND_COLOR, command=next_card)
 right_btn.grid(row=1, column=1)
-wrong_btn = Button(image=wrong_img, highlightthickness=0, background=BACKGROUND_COLOR, command=create_card)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, background=BACKGROUND_COLOR, command=to_learn)
 wrong_btn.grid(row=1, column=0)
 
 # Call Function to create default title and word in canvas texts
-create_card()
+next_card()
 
 window.mainloop()
